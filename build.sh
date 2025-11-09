9a28d3e36cdf87dc7188960bab2a9488c3dacb7e
Viktor Arkhipov <arkhipov.vd@gmail.com>
2025-11-09 19:47:45 +0300

    psql logic added to build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,3 @@
-#!/usr/bin/env bash
-# скачиваем uv и запускаем команду установки зависимостей
 curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
-make install
+make install && psql -a -d $DATABASE_URL -f database.sql
