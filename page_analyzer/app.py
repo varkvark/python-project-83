176c1a874498d066024b5b0326e867b6e29d926b
Viktor Arkhipov <arkhipov.vd@gmail.com>
2025-11-09 19:53:43 +0300

    Add new dependencies in page_analyzer/app.py

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,11 +1,16 @@
+from flask import Flask, request, render_template, redirect, url_for, flash
+import psycopg2
 import os
-from flask import Flask, render_template
+import validators
 from dotenv import load_dotenv
+from .formatting import parsed_url, format_date
+from .db import open_db_connection, close_db_connection, check_if_url_exists, insert_url
+
+
 load_dotenv()
-
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
 
 @app.route('/')
 def home():
