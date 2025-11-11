9cf392faaf3c6c91e827ee50439d92fa8d2c79fd
Viktor Arkhipov <arkhipov.vd@gmail.com>
2025-11-11 21:40:59 +0300

    Added Added database management functions in page_analyzer/db.py

--- page_analyzer/db.py
+++ page_analyzer/db.py
@@ -1,5 +1,7 @@
 import os
 import psycopg2
+import requests
+from bs4 import BeautifulSoup
 from psycopg2 import extras
 from dotenv import load_dotenv
 from datetime import datetime
@@ -28,7 +30,7 @@ def close_db_connection(conn, cur):
         conn.close()
 
 
-def check_url_exists(cur, url):
+def check_if_url_exists(cur, url):
     cur.execute('SELECT id FROM urls WHERE name = %s', (url,))
     return cur.fetchone()
 
@@ -38,7 +40,28 @@ def insert_url(cur, url):
         'INSERT INTO urls (name, created_at) VALUES (%s, %s) RETURNING id',
         (url, datetime.now())
     )
+
     return cur.fetchone()['id']
+
+
+def get_all_urls():
+    conn, cur = open_db_connection()
+    cur.execute('''
+        SELECT
+            u.id,
+            u.name,
+            MAX(c.created_at) AS last_checked,
+            MAX(c.status_code) AS last_status_code
+        FROM urls u
+        LEFT JOIN url_checks c ON u.id = c.url_id
+        GROUP BY u.id
+        ORDER BY u.created_at DESC
+    ''')
+
+    urls_data = cur.fetchall()
+    conn.close()
+
+    return urls_data
 
 
 def get_url_by_id(conn, id):
@@ -47,3 +70,72 @@ def get_url_by_id(conn, id):
     result = cur.fetchone()
     cur.close()
     return result['name'] if result else None
+
+
+def insert_in_url_check(conn, url_id, data):
+    cur = conn.cursor()
+    cur.execute(
+        '''
+        INSERT INTO url_checks (
+            url_id, status_code, h1, title, description, created_at
+        )
+        VALUES (%s, %s, %s, %s, %s, %s)
+        ''',
+        (
+            url_id,
+            data['status_code'],
+            data['h1'],
+            data['title'],
+            data['description'],
+            datetime.now()
+        )
+    )
+
+    conn.commit()
+    cur.close()
+
+
+def extract_data_from_url(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        soup = BeautifulSoup(response.content, 'html.parser')
+        return {
+            'title': (
+                soup.find('title').text
+                if soup.find('title')
+                else None
+            ),
+            'h1': (
+                soup.find('h1').text
+                if soup.find('h1')
+                else None
+            ),
+            'description': (
+                soup.find('meta', attrs={'name': 'description'})['content']
+                if soup.find('meta', attrs={'name': 'description'})
+                else None
+            ),
+            'status_code': response.status_code
+        }
+    except requests.RequestException:
+        return {'error': 'Произошла ошибка при проверке'}
+
+
+def get_url_details(url_id):
+    conn, cur = open_db_connection()
+    cur.execute(
+        'SELECT * FROM urls WHERE id = %s',
+        (url_id,)
+    )
+
+    url_data = cur.fetchone()
+
+    cur.execute(
+        'SELECT * FROM url_checks WHERE url_id = %s ORDER BY created_at DESC',
+        (url_id,)
+    )
+
+    checks = cur.fetchall()
+    close_db_connection(conn, cur)
+    return url_data, checks
