ff36d2b22b0b9b9b5dd867dd652a5fbd5294fa45
Viktor Arkhipov <arkhipov.vd@gmail.com>
2025-11-11 21:38:16 +0300

    Added table "url_checks" configuration in database.sql

--- database.sql
+++ database.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS url_checks;
 DROP TABLE IF EXISTS urls;
 
 
@@ -5,4 +6,15 @@ CREATE TABLE urls (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) UNIQUE NOT NULL,
     created_at TIMESTAMP NOT NULL
+);
+
+
+CREATE TABLE url_checks (
+    id SERIAL PRIMARY KEY,
+    url_id INTEGER REFERENCES urls(id),
+    status_code INTEGER,
+    h1 TEXT,
+    title TEXT,
+    description TEXT,
+    created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
