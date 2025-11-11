9bac94dbf8493bc21817f8582c8f023a8570e522
Viktor Arkhipov <arkhipov.vd@gmail.com>
2025-11-11 21:42:50 +0300

    Added and renamed normalizing functions in page_analyzer/formatting.py

--- page_analyzer/formatting.py
+++ page_analyzer/formatting.py
@@ -3,7 +3,15 @@ from urllib.parse import urlparse, urlunparse
 
 def parsed_url(input_url):
     url_parts = urlparse(input_url)
-    parsed_url = urlunparse((url_parts.scheme, url_parts.netloc, '', '', '', ''))
+    parsed_url = urlunparse((
+        url_parts.scheme,
+        url_parts.netloc,
+        '',
+        '',
+        '',
+        ''
+    ))
+
     return parsed_url
 
 
