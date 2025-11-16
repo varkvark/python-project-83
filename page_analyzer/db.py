e42da4c5c444dede53fdfc160039aa26bc5c8dc1
Viktor Arkhipov <arkhipov.vd@gmail.com>
2025-11-16 23:55:03 +0300

    delet commented func from page_analyzer/db.py

--- page_analyzer/db.py
+++ page_analyzer/db.py
@@ -93,49 +93,6 @@ def insert_in_url_check(conn, url_id, data):
     cur.close()
 
 
-
-
-
-
-
-
-
-'''
-def extract_data_from_url(url):
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-        soup = BeautifulSoup(response.content, 'html.parser')
-        return {
-            'title': (
-                soup.find('title').text
-                if soup.find('title')
-                else None
-            ),
-            'h1': (
-                soup.find('h1').text
-                if soup.find('h1')
-                else None
-            ),
-            'description': (
-                soup.find('meta', attrs={'name': 'description'})['content']
-                if soup.find('meta', attrs={'name': 'description'})
-                else None
-            ),
-            'status_code': response.status_code
-        }
-    except requests.RequestException:
-        return {'error': 'Произошла ошибка при проверке'}
-'''
-
-
-
-
-
-
-
-
-
 def get_url_details(url_id):
     conn, cur = open_db_connection()
     cur.execute(
